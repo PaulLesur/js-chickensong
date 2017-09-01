238e224db4ba85e2a93a693da022f91d9e603be8
Paul <paul.lesur@free.fr>
2017-09-01 15:17:58 +0200

    Ajout d'une TODO liste. Fonctionnalité ajoutée : Ajouter et supprimer une poule

--- js/chicken.js
+++ js/chicken.js
@@ -56,6 +56,10 @@ class Poule {
     }
   }
 
+  detruire(){
+    $("#"+this.id).remove();
+  }
+
   getPosition(){
     return $("#"+this.id).position();
   }
@@ -121,8 +125,12 @@ interact('.poule')
       for (var enc in enclos) {
         if (enclos.hasOwnProperty(enc)) {
           if (enclos[enc].verifPoule(poule)) {
-            poule.chanter(enclos[enc].id);
-            poule.animer(".gif");
+            if (enc==3) { //POUBELLE
+              poule.detruire();
+            }else{
+              poule.chanter(enclos[enc].id);
+              poule.animer(".gif");
+            }
           }
         }
       }
@@ -152,6 +160,18 @@ interact('.poule')
   }
 
 
+function createPoule(){
+
+  var colors = ['blanche', 'marron', 'grise'];
+  var color = colors[Math.floor(Math.random() * colors.length)];
+
+  var poule = new Poule(color);
+  poule.ajouterPoule();
+  poules[poule.id] = poule;
+  (poules[poule.id]).chant1.play();
+  (poules[poule.id]).chant2.play();
+}
+
 
 /*
  * Variables pour le cas de test !
@@ -162,11 +182,14 @@ var poules = {};
 
 var p1 = new Poule("blanche");
 p1.ajouterPoule();
-var p2 = new Poule("marron")
+var p2 = new Poule("marron");
 p2.ajouterPoule();
+var p3 = new Poule("grise");
+p3.ajouterPoule();
 
 poules[p1.id] = p1;
 poules[p2.id] = p2;
+poules[p3.id] = p3;
 
 for (var poule in poules) {
   if (poules.hasOwnProperty(poule)) {
@@ -183,6 +206,10 @@ var enclosMelodies = new Enclos(1, "Mélodies");
 enclosMelodies.ajouter();
 var enclosBeats = new Enclos(2, "Fond Sonore");
 enclosBeats.ajouter();
+var poubelle = new Enclos(3, "Poubelle");
+poubelle.ajouter();
+
 
 enclos[enclosMelodies.id] = enclosMelodies;
 enclos[enclosBeats.id] = enclosBeats;
+enclos[poubelle.id] = poubelle;
---
+++ todo.md
@@ -0,0 +1,19 @@
+# TODO
+Liste des choses, fonctionnalités et idées qui pourraient être apportées par la suite.
+
+
+- [ ] Ajouter une poule
+  - [x] Fonctionnalité
+  - [ ] Animations
+  - [ ] Limite du nombre de poule
+  - [ ]
+
+- [ ] Supprimer une poule
+  - [x] Fonctionnalité
+  - [ ] Animations
+
+- [ ] Ajouter d'autres animaux
+- [ ] Objectifs
+  - [ ] Découvrir des combinaisons de mélodies
+    - Débloque de nouveaux animaux
+- [ ] Acheter des animaux avec des "sous"
